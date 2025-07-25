496ac0a09b10e70dfae8de22c4e071094ee50ada
ShaluKasera <shalukumari93129@gmail.com>
2025-07-25 23:50:34 +0530

    2nd

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import Container from "./Container";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>
-      <Container>footer</Container>
+    <div className="bg-[#212121] text-white">
+      <Container className={"flex flex-row"}>
+        <div>
+          <p>Contact Us</p>
+          <p>
+            If you want to be a part of ICCHE, please feel free to reach out{" "}
+          </p>
+          <p>Slater Hall, IIEST Shibpur, Howrah West Bengal</p>
+        </div>
+        <div>
+          <p>Quick Links</p>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/">Volunteers</Link>
+            </li>
+            <li>
+              <Link href="/">Students</Link>
+            </li>
+            <li>
+              <Link href="/">Alumni</Link>
+            </li>
+            <li>
+              <Link href="/">About us</Link>
+            </li>
+          </ul>
+        </div>
+      </Container>
     </div>
   );
 };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,17 +1,73 @@
-import React from 'react'
-import Container from './Container'
+"use client";
+import React from "react";
+import Container from "./Container";
+import {
+  FaPhoneAlt,
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import Link from "next/link";
+import Button from "../atoms/Button";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Gallery", href: "/gallery" },
+    { name: "Event", href: "/event" },
+    { name: "Donation Drive", href: "/donation-drive" },
+    { name: "About", href: "/about" },
+    { name: "Classroom", href: "/classroom" },
+  ];
+
   return (
     <div>
-      <Container>
-      <div>
-        header
+      <div className="w-full h-[35px] bg-[#FF7043] text-white ">
+        <Container className="flex flex-row justify-between px-10 py-2 text-sm">
+          <div className="flex gap-2 items-center">
+            <FaPhoneAlt />
+            <p>+91 9999999999</p>
+          </div>
+          <div className="flex gap-4">
+            <FaFacebookF />
+            <FaInstagram />
+            <FaTwitter />
+          </div>
+        </Container>
       </div>
+      <Container className="py-2">
+        <div className="flex justify-between">
+          <Link href="/">
+            <p className="text-[#FF7043] font-semibold text-4xl">ICCHE</p>
+            <div className="h-[1.5px] w-[125px] mb-1 bg-black" />
+            <p className="text-[9px]">Non-profitable Organization</p>
+          </Link>
+          <div className="flex items-center">
+            <ul className="flex flex-row gap-5">
+              {navLinks.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={`hover:text-[#FF7043] transition-colors duration-200 ${
+                      pathname === link.href ? "text-[#FF7043]" : "text-black"
+                    }`}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex items-center">
+            <Button text={"Login"} />
+          </div>
+        </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
